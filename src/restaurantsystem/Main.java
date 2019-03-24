58db08f7e20e8402b87e5dc9f625555f3aa4c2fc
Shahin <shahin@uilicious.com>
2019-03-24 19:48:40 +0800

    Create required folder and files during the app bootup

--- src/restaurantsystem/Main.java
+++ src/restaurantsystem/Main.java
@@ -5,6 +5,11 @@
  */
 package restaurantsystem;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
 import restaurantsystem.component.auth.Login;
 
@@ -18,12 +23,40 @@ public class Main extends JFrame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         // At first, show the login page and show menu after - 
         // the authentication process completed
+        
+        createRequiredFileIfDoesNotExist();
+        
         Login im = new Login();
         im.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         im.setVisible(true);
+    }
+    
+    private static void createRequiredFileIfDoesNotExist() {
+        String fileNames [];
+        
+        File rootDir = new File("storage");
+        rootDir.mkdirs();
+        
+        fileNames = new String [] {"storage/item.txt",
+            "storage/labour.txt",
+            "storage/order.txt",
+            "storage/orderLine.txt"};
+        
+        for (String fileName : fileNames) {
+            File file = new File(fileName);
+            if(!file.exists())
+            {  
+                try {
+                    file.createNewFile();
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+       
     }
 
 }
